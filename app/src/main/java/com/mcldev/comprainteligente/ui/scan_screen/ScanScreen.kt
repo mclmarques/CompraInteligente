59469a835b63ca5cfdf87ed86bc59e121c189458
mcl <marcelmr09@gmail.com>
2024-12-29 23:58:04 -0300

    Added improved error screen when launching camera intent

--- app/src/main/java/com/mcldev/comprainteligente/ui/scan_screen/ScanScreen.kt
+++ app/src/main/java/com/mcldev/comprainteligente/ui/scan_screen/ScanScreen.kt
@@ -1,19 +1,29 @@
 package com.mcldev.comprainteligente.ui.scan_screen
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
+import android.provider.Settings
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -25,9 +35,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import java.io.File
@@ -97,12 +111,38 @@ fun ScanScreen(
         }
         // Error message
         if (isCameraError) {
-            Text(
-                text = "Camera app is not available or failed.",
-                style = MaterialTheme.typography.bodyMedium,
-                color = MaterialTheme.colorScheme.error,
-                modifier = Modifier.padding(16.dp)
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxSize() // Occupy the entire screen
+                    .padding(32.dp), // Add padding to avoid edge-to-edge text
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Camera app is not available or failed.",
+                    style = MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center
+                    ),
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp)) // Add spacing between the texts
+                Text(
+                    text = "This is likely because the camera app is disabled. You can enable it in the settings or install another camera app.",
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(64.dp)) // Add spacing before the button
+                Button(
+                    onClick = {
+                        val intent = Intent(Settings.ACTION_SETTINGS)
+                        context.startActivity(intent)
+                    },
+                    modifier = Modifier.align(Alignment.CenterHorizontally) // Center the button horizontally
+                ) {
+                    Text("Open Settings")
+                }
+            }
         }
     }
 }
