6e84dca8fc2480655c08c8af3e8c01fd8865b89b
Marcel <mcldev@gmail.com>
2025-04-09 12:11:30 -0300

    Added SupermarketDaoTest.kt Added a few tests to ProductDaoTest.kt

--- app/src/androidTest/java/com/mcldev/comprainteligente/ProductDaoTest.kt
+++ app/src/androidTest/java/com/mcldev/comprainteligente/ProductDaoTest.kt
@@ -8,6 +8,7 @@ import com.mcldev.comprainteligente.data.dao.SupermarketDao
 import com.mcldev.comprainteligente.data.database.DataBase
 import com.mcldev.comprainteligente.data.entities.Product
 import com.mcldev.comprainteligente.data.entities.Supermarket
+import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert
@@ -16,6 +17,15 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.TimeUnit
 
+/**
+ * The idea here is to perform basic checks around the backend. Here is tested the following items:
+ * Upsert a product
+ * Search by name a product
+ * Time based delete method (only the method, the worker and other components are NOT tested)
+ * Upsert an existing product
+ * Try to add a product linked to non existing supermarket
+ * In general edge cases or wrong values aren't tested here as they should be checked by the ViewModel (ScanScreenVM.kt)
+ */
 @RunWith(AndroidJUnit4::class)
 class ProductDaoTest {
 
@@ -82,5 +92,47 @@ class ProductDaoTest {
         val allProducts = productDao.getAllProducts()
         Assert.assertEquals(1, allProducts.size)
         Assert.assertEquals("Fresh", allProducts.first().name)
+    }
+    @Test
+    fun upsertExistingProduct_shouldUpdateFields() = runBlocking {
+        val supermarket = Supermarket(name = "Update Mart", averagePrice = 0.0f)
+        supermarketDao.upsertSupermarket(supermarket)
+        val s = supermarketDao.getSupermarketByName("Update Mart")!!
+
+        // First insert
+        val product = Product(
+            name = "Rice",
+            price = 5.0f,
+            unit = "kg",
+            supermarketId = s.id,
+            date = System.currentTimeMillis()
+        )
+        productDao.upsertProduct(product)
+
+        // Fetch inserted product to get its generated ID
+        val initial = productDao.getProductsByName("Rice").first()
+
+        // Modify and re-upsert
+        val updated = initial.copy(price = 6.0f, unit = "g")
+        productDao.upsertProduct(updated)
+
+        val result = productDao.getProductsByName("Rice").first()
+        assertEquals(initial.id, result.id)
+        assertEquals(6.0f, result.price)
+        assertEquals("g", result.unit)
+    }
+
+    @Test(expected = android.database.sqlite.SQLiteConstraintException::class)
+    fun insertProductWithInvalidSupermarket_shouldThrowException() = runBlocking {
+        val product = Product(
+            name = "Ghost",
+            price = 1.0f,
+            unit = "unit",
+            supermarketId = 9999, // doesn't exist
+            date = System.currentTimeMillis()
+        )
+
+        // This should fail due to foreign key constraint
+        productDao.upsertProduct(product)
     }
 }
