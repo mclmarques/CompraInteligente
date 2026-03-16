7e2a6cfebff573671948170fd22cdbef39e5b25d
Marcel <marcelmr09@gmail.com>
2026-03-16 19:55:56 -0300

    AGP Upgrade Dependencies updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp") version "2.2.0-2.0.2"
+    id("com.google.devtools.ksp") version "2.3.2"
     alias(libs.plugins.kotlin.serialization)
 }
 
